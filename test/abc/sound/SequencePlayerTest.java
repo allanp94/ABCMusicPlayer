6246c4d1ec071048e7e3f743f275b6e7def0f482
shanespecht <51672002+shanespecht@users.noreply.github.com>
2022-11-23 09:20:51 -0600

    commented what notes they should be

--- test/abc/sound/SequencePlayerTest.java
+++ test/abc/sound/SequencePlayerTest.java
@@ -12,7 +12,7 @@ public class SequencePlayerTest {
     public void piece1Test() throws InterruptedException {
         try {
             SequencePlayer player = new SequencePlayer(120, 12);
-
+/*
             player.addNote(new Pitch('C').toMidiNote(), 0, 12);
             player.addNote(new Pitch('C').toMidiNote(), 12, 12);
             player.addNote(new Pitch('C').toMidiNote(), 24, 9);
@@ -44,7 +44,7 @@ public class SequencePlayerTest {
             player.addNote(new Pitch('E').toMidiNote(), 156, 9);
             player.addNote(new Pitch('D').toMidiNote(), 165, 3);
             player.addNote(new Pitch('C').toMidiNote(), 168, 24);
-
+*/
             System.out.println(player);
             
             player.play();
@@ -63,49 +63,49 @@ public class SequencePlayerTest {
         try {
             SequencePlayer player = new SequencePlayer(120, 12);
 
-            player.addNote(new Pitch('C').toMidiNote(), 0, 6);
-            player.addNote(new Pitch('C').toMidiNote(), 0, 6);
-            player.addNote(new Pitch('C').toMidiNote(), 6, 6);
-            player.addNote(new Pitch('C').toMidiNote(), 6, 6);
-            player.addNote(new Pitch('C').toMidiNote(), 12, 6);
-            player.addNote(new Pitch('D').toMidiNote(), 18, 6);
-            player.addNote(new Pitch('D').toMidiNote(), 18, 6);
-            player.addNote(new Pitch('E').toMidiNote(), 24, 6);
-            player.addNote(new Pitch('E').toMidiNote(), 30, 6);
-            player.addNote(new Pitch('E').toMidiNote(), 30, 6);
-            player.addNote(new Pitch('E').toMidiNote(), 36, 12);
-            player.addNote(new Pitch('E').toMidiNote(), 36, 12);
+            player.addNote(new Pitch('C').toMidiNote(), 0, 6); //e^
+            player.addNote(new Pitch('C').toMidiNote(), 0, 6); //F
+            player.addNote(new Pitch('C').toMidiNote(), 6, 6); //e^
+            player.addNote(new Pitch('C').toMidiNote(), 6, 6); //F
+            player.addNote(new Pitch('C').toMidiNote(), 12, 6); //z
+            player.addNote(new Pitch('D').toMidiNote(), 18, 6); //e^
+            player.addNote(new Pitch('D').toMidiNote(), 18, 6); //F
+            player.addNote(new Pitch('E').toMidiNote(), 24, 6); //z
+            player.addNote(new Pitch('E').toMidiNote(), 30, 6); //c^
+            player.addNote(new Pitch('E').toMidiNote(), 30, 6); //F
+            player.addNote(new Pitch('E').toMidiNote(), 36, 12); //e^
+            player.addNote(new Pitch('E').toMidiNote(), 36, 12); //F
+            
+            player.addNote(new Pitch('E').toMidiNote(), 48, 12); //g
+            player.addNote(new Pitch('E').toMidiNote(), 48, 12); //B
+            player.addNote(new Pitch('E').toMidiNote(), 48, 12); //G
+            player.addNote(new Pitch('D').toMidiNote(), 60, 12); //z
+            player.addNote(new Pitch('E').toMidiNote(), 72, 12); //G
+            player.addNote(new Pitch('F').toMidiNote(), 84, 12); //z
 
-            player.addNote(new Pitch('E').toMidiNote(), 48, 12);
-            player.addNote(new Pitch('E').toMidiNote(), 48, 12);
-            player.addNote(new Pitch('E').toMidiNote(), 48, 12);
-            player.addNote(new Pitch('D').toMidiNote(), 60, 12);
-            player.addNote(new Pitch('E').toMidiNote(), 72, 12);
-            player.addNote(new Pitch('F').toMidiNote(), 84, 12);
+            player.addNote(new Pitch('C').transpose(12).toMidiNote(), 96, 18); //c
+            player.addNote(new Pitch('C').transpose(12).toMidiNote(), 114, 6); //G
+            player.addNote(new Pitch('C').transpose(12).toMidiNote(), 120, 12); //z
+            player.addNote(new Pitch('C').transpose(12).toMidiNote(), 132, 12); //E
 
-            player.addNote(new Pitch('C').transpose(12).toMidiNote(), 96, 18);
-            player.addNote(new Pitch('C').transpose(12).toMidiNote(), 114, 6);
-            player.addNote(new Pitch('C').transpose(12).toMidiNote(), 120, 12);
-            player.addNote(new Pitch('C').transpose(12).toMidiNote(), 132, 12);
+            player.addNote(new Pitch('E').toMidiNote(), 144, 6); //E
+            player.addNote(new Pitch('E').toMidiNote(), 156, 12); //A
+            player.addNote(new Pitch('E').toMidiNote(), 128, 12); //B
+            player.addNote(new Pitch('C').toMidiNote(), 132, 6); //B_
+            player.addNote(new Pitch('C').toMidiNote(), 136, 12); //A
 
-            player.addNote(new Pitch('E').toMidiNote(), 144, 6);
-            player.addNote(new Pitch('E').toMidiNote(), 156, 12);
-            player.addNote(new Pitch('E').toMidiNote(), 128, 12);
-            player.addNote(new Pitch('C').toMidiNote(), 132, 6);
-            player.addNote(new Pitch('C').toMidiNote(), 136, 12);
+            player.addNote(new Pitch('E').toMidiNote(), 144, 8); //G
+            player.addNote(new Pitch('E').toMidiNote(), 156, 8); //e
+            player.addNote(new Pitch('E').toMidiNote(), 128, 8); //g
+            player.addNote(new Pitch('C').toMidiNote(), 132, 12); //a
+            player.addNote(new Pitch('C').toMidiNote(), 136, 6); //f
+            player.addNote(new Pitch('C').toMidiNote(), 136, 6); //g
 
-            player.addNote(new Pitch('E').toMidiNote(), 144, 8);
-            player.addNote(new Pitch('E').toMidiNote(), 156, 8);
-            player.addNote(new Pitch('E').toMidiNote(), 128, 8);
-            player.addNote(new Pitch('C').toMidiNote(), 132, 12);
-            player.addNote(new Pitch('C').toMidiNote(), 136, 6);
-            player.addNote(new Pitch('C').toMidiNote(), 136, 6);
-
-            player.addNote(new Pitch('E').toMidiNote(), 144, 6);
-            player.addNote(new Pitch('E').toMidiNote(), 156, 12);
-            player.addNote(new Pitch('E').toMidiNote(), 128, 6);
-            player.addNote(new Pitch('C').toMidiNote(), 132, 6);
-            player.addNote(new Pitch('C').toMidiNote(), 136, 18);
+            player.addNote(new Pitch('E').toMidiNote(), 144, 6); //z
+            player.addNote(new Pitch('E').toMidiNote(), 156, 12); //e
+            player.addNote(new Pitch('E').toMidiNote(), 128, 6); //c
+            player.addNote(new Pitch('C').toMidiNote(), 132, 6); //d
+            player.addNote(new Pitch('C').toMidiNote(), 136, 18); //B
 
             System.out.println(player);
 
