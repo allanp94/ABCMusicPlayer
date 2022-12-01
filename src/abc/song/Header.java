54f1c6d05a9263ab150dacb1a7f1fc0c83a659e2
dmiska25 <73676469+dmiska25@users.noreply.github.com>
2022-11-30 22:02:30 -0600

    Refactors some header fields, adds some defaults for header fields

--- src/abc/song/Header.java
+++ src/abc/song/Header.java
@@ -4,38 +4,55 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Header {
-
-	// TODO: may need to parse further than string prior to object
 	private String composerName; // C
 	private String keySignature; // K
-	private String defaultLength; // L
-	private String meter; // M
-	private String tempo; // Q
+	private Float length; // L
+	private Float meter; // M
+	private Float tempoLength; // Q
+	private Integer tempo; // Q
 	private String title; // T
 	private List<String> voices; // V
 	
+	private static final float DEFAULT_METER = 1.0f;
+	private static final int DEFAULT_TEMPO = 100;
+	private static final String DEFAULT_COMPOSER_NAME = "Unknown";
+	private final DefaultLength DEFAULT_LENGTH = () -> {
+		if (this.meter == null) throw new IllegalStateException("Meter must be defined to set default length");
+		return meter < .75 ? 1/16f : 1/8f;
+	};
+	
+	/*
+	 * Get a Header object with the given fields
+	 * Title and Key Signature are required to be non null. All other parameters allow null.
+	 */
 	public Header(
 				String composerName,
 				String keySignature,
-				String defaultLength,
-				String meter,
-				String tempo,
+				Float length,
+				Float meter,
+				Float tempoLength,
+				Integer tempo,
 				String title,
 				List<String> voices
 			) {
 		this.setComposerName(composerName);
 		this.setKeySignature(keySignature);
-		this.setDefaultLength(defaultLength);
+		this.setLength(length);
 		this.setMeter(meter);
+		this.setTempoLength(tempoLength);
 		this.setTempo(tempo);
 		this.setTitle(title);
 		this.setVoices(voices);
 	}
 	
+	/*
+	 * Get a copy of a given header object
+	 * Title and Key Signature are required to be non null. All other parameters can be passed as null.
+	 */
 	public Header(Header headerToCopy) {
 		this.setComposerName(headerToCopy.getComposerName());
 		this.setKeySignature(headerToCopy.getKeySignature());
-		this.setDefaultLength(headerToCopy.getDefaultLength());
+		this.setLength(headerToCopy.getLength());
 		this.setMeter(headerToCopy.getMeter());
 		this.setTempo(headerToCopy.getTempo());
 		this.setTitle(headerToCopy.getTitle());
@@ -47,6 +64,10 @@ public class Header {
 	}
 
 	public void setComposerName(String composerName) {
+		if (composerName == null) {
+			this.composerName = Header.DEFAULT_COMPOSER_NAME;
+			return;
+		}
 		this.composerName = composerName;
 	}
 
@@ -55,30 +76,44 @@ public class Header {
 	}
 
 	public void setKeySignature(String keySignature) {
+		if (keySignature == null) throw new IllegalArgumentException("Key Signature cannot be null");
 		this.keySignature = keySignature;
 	}
 
-	public String getDefaultLength() {
-		return defaultLength;
+	public Float getLength() {
+		return length;
 	}
 
-	public void setDefaultLength(String defaultLength) {
-		this.defaultLength = defaultLength;
+	public void setLength(Float length) {
+		if (length == null) {
+			this.length = this.DEFAULT_LENGTH.get();
+			return;
+		}
+		this.length = length;
 	}
 
-	public String getMeter() {
+	public Float getMeter() {
 		return meter;
 	}
 
-	public void setMeter(String meter) {
+	public void setMeter(Float meter) {
+		if (meter == null) {
+			this.meter = Header.DEFAULT_METER;
+			return;
+		}
+		
 		this.meter = meter;
 	}
 
-	public String getTempo() {
+	public Integer getTempo() {
 		return tempo;
 	}
 
-	public void setTempo(String tempo) {
+	public void setTempo(Integer tempo) {
+		if (tempo == null) {
+			this.tempo = Header.DEFAULT_TEMPO;
+			return;
+		}
 		this.tempo = tempo;
 	}
 
@@ -87,6 +122,7 @@ public class Header {
 	}
 
 	public void setTitle(String title) {
+		if (title == null) throw new IllegalArgumentException("Title cannot be null");
 		this.title = title;
 	}
 
@@ -101,14 +137,27 @@ public class Header {
 	public void addVoice(String voice) {
 		this.voices.add(voice);
 	}
+
+	public Float getTempoLength() {
+		return tempoLength;
+	}
+	
+	public void setTempoLength(Float tempoLength) {
+		this.tempoLength = tempoLength;
+	}
 	
 	public String toString() {
 		return "Composer Name: " + composerName + "\n" +
 				"Key Signature: " + keySignature + "\n" +
-				"Default Length: " + defaultLength + "\n" +
+				"Default Length: " + length + "\n" +
 				"Meter: " + meter + "\n" +
 				"Tempo: " + tempo + "\n" +
 				"Title: " + title + "\n" +
 				"Voices: " + voices + "\n";
 	}
+
+}
+
+interface DefaultLength{
+	public Float get();
 }
