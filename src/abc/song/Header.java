653ff273aa3173b83a69019f882e2df5ec1550d3
allan.pirillis <allan.p94@gmail.com>
2022-12-14 11:03:36 -0600

    set a default tempo length in Header.java to 100

--- src/abc/song/Header.java
+++ src/abc/song/Header.java
@@ -19,6 +19,7 @@ public class Header {
 	private static final float DEFAULT_METER = 1.0f;
 	private static final int DEFAULT_TEMPO = 100;
 	private static final String DEFAULT_COMPOSER_NAME = "Unknown";
+	private static final float DEFAULT_TEMPO_LENGTH = 100;
 	private final DefaultLength DEFAULT_LENGTH = () -> {
 		if (this.meter == null) throw new IllegalStateException("Meter must be defined to set default length");
 		return meter < .75 ? 1/16f : 1/8f;
@@ -150,6 +151,10 @@ public class Header {
 	}
 	
 	public void setTempoLength(Float tempoLength) {
+		if (tempoLength == null) {
+			this.tempoLength = Header.DEFAULT_TEMPO_LENGTH; 
+			return; 
+		}
 		this.tempoLength = tempoLength;
 	}
 	
